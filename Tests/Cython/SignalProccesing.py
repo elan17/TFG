d35140e4ed9f2a0a6133defe7339556ef4d25127
Juan Toca <elan17.programacion@gmail.com>
2020-07-17 16:32:47 +0200

    Fixed the test suite for the SignalProccesing module

--- Tests/Cython/SignalProccesing.py
+++ Tests/Cython/SignalProccesing.py
@@ -4,6 +4,8 @@ from hypothesis import given, assume, seed
 import hypothesis.strategies as st
 import hypothesis.extra.numpy as hnp
 
+from math import gcd
+
 from time import sleep
 
 import numpy as np
@@ -12,34 +14,22 @@ sys.path.append("../../Project")
 
 import Cython_lib.SignalProccesing as SP
 
-precision_margin = 10.0**(-200)
-def soft_array_compare(arr1, arr2):
+def soft_array_compare(arr1, arr2, precision_margin=10**(-10)):
     """
     Let's us compare arrays without carying of floating point precision
     """
     dif = np.absolute(arr1 - arr2)
     comp = max(dif) <= precision_margin
-    if not comp:
-        print(arr1)
-        print(arr2)
-        print(dif)
-        exit()
     return comp
 
 @given(st.data())
-@seed(102411555924360395765910700497704526526)
 def test_composite_autocorrelation(data):
-    small_int = st.integers(min_value=2, max_value=5)
+    small_int = st.integers(min_value=2, max_value=25)
     signal_length = data.draw(small_int)
     shifts_length = data.draw(small_int)
-    assume(signal_length % shifts_length != 0)
-    assume(shifts_length % signal_length != 0)
+    assume(gcd(signal_length, shifts_length) == 1) # Check if they are coprimes
     signal = data.draw(hnp.arrays(np.int64, signal_length, elements=st.integers(min_value=-1, max_value=1)))
     shifts = data.draw(hnp.arrays(np.int32, shifts_length, elements=st.integers(min_value=0, max_value=max(signal_length-1, 0))))
-
-    print(signal)
-    print(shifts)
-
     arr1 = SP.autocorrelation(SP.composition(signal, shifts))
     arr2 = SP.composite_autocorrelation(signal, shifts)
     assert soft_array_compare(arr1, arr2)
