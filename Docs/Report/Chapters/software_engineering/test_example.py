c6898e9e5ce4958ed4ba35c20329dec54a0e07f3
Juan Toca <elan17.programacion@gmail.com>
2020-08-02 12:08:12 +0200

    Added an example of property testing

---
+++ Docs/Report/Chapters/software_engineering/test_example.py
@@ -0,0 +1,25 @@
+import Cython_lib.SignalProccesing as SP
+import unittest as ut
+
+from hypothesis import given
+import hypothesis.strategies as st
+import hypothesis.extra.numpy as hnp
+
+class TestAutocorrelation(ut.TestCase):
+
+    def generator(self, data):
+        small_int = st.integers(min_value=0, max_value=25)
+        signal_length = data.draw(small_int)
+        return data.draw(hnp.arrays(np.int64, signal_length, elements=st.integers(min_value=-1, max_value=1)))
+
+    @given(st.data())
+    def test_maximum_value(self, data):
+        """
+        Test that checks the maximum value of the autocorrelation
+        """
+        signal = self.generator(data)
+        auto = SP.autocorrelation(signal)
+        if not len(signal):
+            return
+        assert auto[0]-1 < max(auto) < auto[0]+1
+        assert max(auto) < len(auto)+1
