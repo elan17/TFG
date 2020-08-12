46e6a36a9420c7e3a2a419e43f5ce4fd2a19aada
Juan Toca <elan17.programacion@gmail.com>
2020-08-12 19:15:10 +0200

    Added gold implementation example

---
+++ Docs/Report/Chapters/PRN_generation/example_gold.py
@@ -0,0 +1,16 @@
+def gold_code(maximal1, maximal2):
+    r = [maximal1, maximal2]
+    for x in range(len(maximal2)):
+        r.append(hadamard_product(maximal1, displace(maximal2, x)))
+    return r
+
+def decimation(sequence, k):
+    l = []
+    for x in range(len(sequence)):
+        l.append(sequence[(x*k) % len(sequence)])
+    return vector(l)
+
+def gold_with_decimation(n):
+    s1 = maximal_sequence(n)
+    s2 = decimation(s1, 3) # 5 = (2^k) + 1 when k is 1(1 is coprime with every number)
+    return gold_code(s1, s2)
