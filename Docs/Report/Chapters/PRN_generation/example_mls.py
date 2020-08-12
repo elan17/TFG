d7f4ceb63313a9123c891316e3f72de02e50fd5e
Juan Toca <elan17.programacion@gmail.com>
2020-08-12 19:00:11 +0200

    Updated example of maximal sequences

--- Docs/Report/Chapters/PRN_generation/example_mls.py
+++ Docs/Report/Chapters/PRN_generation/example_mls.py
@@ -1,4 +1,5 @@
 def maximal_sequence(n):
-    F.<alpha> = GF(2**n)
-    g = F.primitive_element()
-    return vector([(g^i).trace() for i in range(2**n-1)])
+    f.<alpha> = GF(2**n)
+    g = f.primitive_element()
+    r = vector([(g^i).trace () for i in range(f.order()-1)]).change_ring(ZZ)
+    return vector([x*2 - 1 for x in r])
