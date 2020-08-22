bc41e482725edea1258ec701d0d72a87da44f438
Juan Toca <elan17.programacion@gmail.com>
2020-08-22 19:22:31 +0200

    Added comments to parallelism.py

--- Project/src/parallelism.py
+++ Project/src/parallelism.py
@@ -23,19 +23,19 @@ def kill_slaves():
         comm.isend(-1, dest=x, tag=11)
 
 def master( polling_delay # Time between polls
-          , shift_length
-          , task_size
-          , hamming
-          , max_value
+          , shift_length # Length of the shift sequence
+          , task_size # Height of the sub trees
+          , hamming # Max hamming autocorrelation
+          , max_value # Length of the base sequence
           , verbose):
     requests = []
-    counter_deallocation = num_proccess - 1
     arr = np.array([-x for x in range(shift_length)], dtype=np.int32)
+    # Task iterator
     task_iter = task_iterator(arr, 1, task_size, max_value, hamming)
     try:
         for x in range(1, num_proccess): # Initialize first task
             requests.append(comm.issend(shift_to_int(next(task_iter), task_size), dest=x, tag=11))
-    except StopIteration:
+    except StopIteration: # If there are not enough tasks
         kill_slaves()
         sleep(0.3)
         print("The number of tasks found where too low for the number of threads, "\
@@ -43,49 +43,52 @@ def master( polling_delay # Time between polls
         exit(0)
 
 
-    for task in task_iter:
+    for task in task_iter: # Iterate over all the tasks
         exit_var = False
         while not exit_var:
             i, b, msg = MPI.Request.testany(requests)
-            if b and i >= 0:
+            if b and i >= 0: # If there is a finished task
+                # assign new task
                 requests[i] = comm.issend(shift_to_int(task, task_size), dest=i+1, tag=11)
                 if verbose:
                     print(i+1, task)
                 exit_var = True
-    while counter_deallocation > 0:
-        i, b, msg = MPI.Request.testany(requests)
-        if b and i >= 0:
-            comm.isend(-1, dest=i+1, tag=11) # Terminate thread
-            counter_deallocation -= 1
+    # Tell the slaves to exit
+    kill_slaves()
 
 def slave( base_sequence
          , sequence_length # Length of shifts sequences
-         , task_size
+         , task_size # Height of the sub trees
          , hamming_upper_limit
          , correlation_upper_limit
          , verbose):
     exit_var = False
     while not exit_var:
+        # Recieve task
         data = comm.recv(source=0, tag=11)
-        if data != -1:
+        if data != -1: # If it's an actual task
+            # Compute the task
             seq = int_to_shift_sequence(data, sequence_length, len(base_sequence)+1, task_size)
             r = bb.py_get_list_of_good_shifts( base_sequence, hamming_upper_limit
                                              , correlation_upper_limit, seq, task_size)
+            # Store the results
             store_sequences(r, rank)
-        else:
+        else: # If there's no more tasks
             if verbose:
                 print(rank, "EXITED")
+            # Exit
             exit_var = True
 
 def task_iterator(arr, current_offset, task_size, max_value, hamming):
     it = len(arr) - task_size
     ham_auto = SP.max_hamming_autocorrelation(arr) < hamming
-    if ham_auto:
-        if current_offset == it:
+    if ham_auto: # Prune
+        if current_offset == it: # base case
             yield arr
-        else:
-            for x in range(max_value+1):
+        else: # recursive case
+            for x in range(max_value+1): # For all possible shifts
                 arr[current_offset] = x
+                # Go one step deeper  
                 yield from task_iterator(arr, current_offset+1, task_size, max_value, hamming)
             arr[current_offset] = -current_offset
 
