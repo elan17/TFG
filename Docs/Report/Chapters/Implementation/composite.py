be8970ffddaeaf17c9a17973200e85f027d8bfb8
Juan Toca <elan17.programacion@gmail.com>
2020-09-08 01:47:13 +0200

    Final version

--- Docs/Report/Chapters/Implementation/composite.py
+++ Docs/Report/Chapters/Implementation/composite.py
@@ -1,19 +1,17 @@
-cdef bint c_good_composite_autocorrelation( int* autocorrelation
-                                          , int l_signal
-                                          , int* shifts
-                                          , int l_shifts
-                                          , int threshold):
-  cdef int output_size = l_signal * l_shifts
-  cdef int output
-  cdef int x, y, affected_column, current_shift, final_shift, positive_difference
-  for x in range(1, output_size):
-    output = 0
-    positive_difference = l_signal - (x%l_signal)
-    for y in range(l_shifts):
-      affected_column =  shifts[(y+x)%l_shifts]
-      current_shift  =  (positive_difference + shifts[y]) % l_signal
-      final_shift = abs(current_shift-affected_column)
-      output = output + autocorrelation[final_shift]
-    if output > threshold:
-      return False
-  return True
+cdef int * composite_with_autocorrelation( int* autocorrelation
+                                         , int l_signal
+                                         , int* shifts
+                                         , int l_shifts):
+    cdef int output_size = l_signal * l_shifts
+    cdef int* output = <int *> malloc(output_size*sizeof(int))
+    cdef int x, y, affected_column, current_shift, final_shift, positive_difference
+    cdef int constant_offset = l_signal
+    for x in range(output_size):
+      output[x] = 0
+      positive_difference = l_signal - (x%l_signal)
+      for y in range(l_shifts):
+        affected_column =  shifts[(y+x)%l_shifts]
+        current_shift  =  (positive_difference + shifts[y]) % l_signal
+        final_shift = abs(current_shift-affected_column)
+        output[x] = output[x] + autocorrelation_with_constant[final_shift]
+    return output
---
+++ Docs/Report/Chapters/Implementation/partial_composite.py
@@ -0,0 +1,20 @@
+cdef bint c_good_composite_autocorrelation( int* autocorrelation
+                                          , int l_signal
+                                          , int* shifts
+                                          , int l_shifts
+                                          , int threshold):
+  cdef int output_size = l_signal * l_shifts
+  cdef int output
+  cdef int x, y, affected_column, current_shift, final_shift, positive_difference
+  cdef int constant_offset = l_signal
+  for x in range(1, output_size):
+    output = 0
+    positive_difference = l_signal - (x%l_signal)
+    for y in range(l_shifts):
+      affected_column =  shifts[(y+x)%l_shifts]
+      current_shift  =  (positive_difference + shifts[y]) % l_signal
+      final_shift = abs(current_shift-affected_column)
+      output = output autocorrelation[final_shift]
+    if output > threshold:
+      return False
+  return True
